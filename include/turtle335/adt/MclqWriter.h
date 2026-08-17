a5895d1301e0c9fd453c8c1297efe738abb83a90
Nansen <898921811@qq.com>
2026-08-17 23:39:55 +0800

    fix: document raw reversed MCLQ FourCC

--- include/turtle335/adt/MclqWriter.h
+++ include/turtle335/adt/MclqWriter.h
@@ -11,6 +11,7 @@ using LegacyMclqPayloadBytes = std::array<std::uint8_t, 804>;
 using LegacyMclqChunkBytes = std::array<std::uint8_t, 812>;
 
 LegacyMclqPayloadBytes SerializeLegacyMclqPayload(const LegacyMclq& mclq);
+// Returns raw ADT bytes; logical MCLQ is stored on disk as FourCC QLCM.
 LegacyMclqChunkBytes SerializeLegacyMclqChunk(const LegacyMclq& mclq);
 
 } // namespace turtle335::adt
